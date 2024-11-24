2e1c1bbba17a6d9c14244b77026389bb7d073046
ua.lifesheets <ua.lifesheets@gmail.com>
2024-11-24 02:07:37 +0200

    Autoload directory to contain files with functions.

--- platform/requires/functions.php
+++ platform/requires/functions.php
@@ -1,1 +1,13 @@
 <?php
+
+# Відкриваємо директорію, яка містить файли з функціями.
+$data = opendir(ROOT . '/platform/functions');
+
+# Перебираємо всі файли в директорії.
+while ($function = readdir($data)) {
+    # Перевіряємо, чи файл має розширення `.php`.
+    if (preg_match('#\.php$#i', $function)) {
+        # Підключаємо файл, що відповідає умові, до основного скрипту.
+        require_once(ROOT . '/platform/functions/' . $function);
+    }
+}
